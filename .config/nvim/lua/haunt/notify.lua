192df472aefe65df580fbe32c247b573c35bbc9a
hnt <dpr@keemail.me>
2025-11-05 10:47:41 -0300

    haunt dotfiles

--- .config/nvim/lua/haunt/notify.lua
+++ .config/nvim/lua/haunt/notify.lua
@@ -7,7 +7,7 @@ notify.setup({
   stages = "fade_in_slide_out",
   fps = 60,
   timeout = 2500,
-  background_colour = "#000000",
+  background_colour = "Normal",
   render = "default",
   top_down = true,
   max_width = 60,
@@ -18,3 +18,42 @@ vim.notify = notify
 vim.schedule(function()
   vim.notify = require("notify")
 end)
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    local links = {
+      NotifyBackground = "Normal",
+
+      NotifyINFOBorder = "FloatBorder",
+      NotifyINFOTitle = "Title",
+      NotifyINFOIcon = "Normal",
+      NotifyINFOBody = "Normal",
+
+      NotifyWARNBorder = "FloatBorder",
+      NotifyWARNTitle = "Title",
+      NotifyWARNIcon = "WarningMsg",
+      NotifyWARNBody = "Normal",
+
+      NotifyERRORBorder = "FloatBorder",
+      NotifyERRORTitle = "Title",
+      NotifyERRORIcon = "ErrorMsg",
+      NotifyERRORBody = "Normal",
+
+      NotifyDEBUGBorder = "FloatBorder",
+      NotifyDEBUGTitle = "Title",
+      NotifyDEBUGIcon = "Comment",
+      NotifyDEBUGBody = "Normal",
+
+      NotifyTRACEBorder = "FloatBorder",
+      NotifyTRACETitle = "Title",
+      NotifyTRACEIcon = "Comment",
+      NotifyTRACEBody = "Normal",
+    }
+
+    for group, link in pairs(links) do
+      vim.api.nvim_set_hl(0, group, { link = link, default = false })
+    end
+  end,
+})
+
+vim.cmd("doautocmd ColorScheme")
